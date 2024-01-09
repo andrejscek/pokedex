a5a13ea11668406ce50902a50bcaec8428003a25
andrejscek <scek.andrej@gmail.com>
2024-01-09 09:44:05 +0100

    work on pokedex

---
+++ com_catch.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+func commandCatch(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a pokemon name")
+	}
+
+	name := args[0]
+	pokemon, err := cfg.pokeapiClient.GetPokemon(name)
+	if err != nil {
+		return err
+	}
+
+	res := rand.Intn(pokemon.BaseExperience)
+
+	fmt.Printf("Trowing Pokebal to catch %s...\n", pokemon.Name)
+	if res > 40 {
+		fmt.Println("Pokemon escaped!")
+		return nil
+	}
+
+	fmt.Printf("%s caught!\n", pokemon.Name)
+	cfg.caughtPokemon[pokemon.Name] = pokemon
+
+	return nil
+
+}
---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,61 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	cacheMap map[string]cacheEntry
+	mutex    *sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) Cache {
+	cache := Cache{
+		cacheMap: make(map[string]cacheEntry),
+		mutex:    &sync.Mutex{},
+	}
+	go cache.reapLoop(interval)
+	return cache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.cacheMap[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	entry, ok := c.cacheMap[key]
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	for k, v := range c.cacheMap {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.cacheMap, k)
+		}
+	}
+}
